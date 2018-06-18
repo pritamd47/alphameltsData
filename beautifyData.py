037576468718dde513b1f56ec042641c12dc1495
Pritam <pritamd47@gmail.com>
2018-06-18 19:20:27 +0530

    Major resetructure, more robust, can now read two types of phaseMain output

--- beautifyData.py
+++ beautifyData.py
@@ -4,25 +4,36 @@ from datetime import datetime as dt
 import os
 import sys
 import getopt
+import re
+from tkinter import filedialog
+from tkinter import *
 
 
-def returnCols(line1, line2):
-    """Reads the first two lines and determines the columns that the DataFrame 
-    object will have
+def returnCols(tbl, line1, line2):
+    """Returns the appropriate columns based on the table which is currently 
+    being read
     
     Arguments:
+        tbl{int} -- takes a value between 1-8 based on the convention used to
+            represent the various table files
         line1 {str} -- First non blank Line
         line2 {str} -- Second non blank Line
     """
-    
-    # Stripping extra characters from line string
-    line1 = line1.strip()
+    # Guide:
+    # 1. phase_main_tbl.txt (type 1) -- Starts with liquid_0 thermodynamic..
+    # 2. phase_main_tbl.txt (type 2) -- Starts with P T ...
+    # 3. phase_mass_tbl.txt -- starts with Phase Masses
+    # 4. phase_vol_tbl.txt -- starts with Phase Volumes
+    # 5. solid_comp_tbl.txt -- starts with Solid Composition
+    # 6. system_mail_tbl.txt -- starts with System Thermodynamic data
+    # 7. trace_main_tbl.txt -- starts as P Pval T Tval
+    # 8. bulk_comp_tbl.txt -- starts with Bulk Composition
 
-    if "System Thermodynamic Data" in line1 or "Bulk Composition" in line1:
+    if tbl == 1:
         line2 = [x.strip() for x in line2.split(' ')]
-        line2 = list(filter(None, line2))
         columns = line2
-    else:
+        columns.insert(2, "Phase")
+    elif tbl == 2:
         line1 = line1.split(' ')
         oxides = line1[4:]
         columns = [
@@ -42,11 +53,37 @@ def returnCols(line1, line2):
                 oxides[-1].strip('\n'),
                 'Mg#',
         ]
+    elif tbl in (3, 4, 5, 6, 8):
+        line2 = [x.strip() for x in line2.split(' ')]
+        columns = line2
+        columns = list(filter(lambda x: x != '', columns))
+    elif tbl == 7:
+        print("Trace Elements not implemented yet")
 
     return columns
 
 
-def _extractPhaseData(iteration, f, columns):
+def _extractPhaseMainT1(phase):
+    phaseName = phase[0].split(" ")[0]
+    
+    line1 = phase.popleft()
+    line2 = phase.popleft()
+    
+    columns = returnCols(1, line1, line2)
+    
+    rawData = []
+    
+    for line in phase:
+        PTcond = [x.strip() for x in line.split(" ")]
+        PTcond.insert(2, phaseName)
+        rawData.append(PTcond)
+    
+    DF = pd.DataFrame(data=rawData, columns=columns)
+    
+    return DF
+
+
+def _extractPhaseDataT2(iteration, f, columns):
     """extracts the data from a single PT condition and returns a dataframe object containing the data
     
     Arguments:
@@ -138,33 +175,94 @@ def _extractPhaseData(iteration, f, columns):
     return pd.DataFrame(data=currentEnv, columns=columns)
 
 
-def findDescriptor(obj):
-    while ("Title" in obj[0] or len(obj[0])<4):
-        obj.popleft()
+def extractSolidComp(path):
+    """Reads a table file which as a generic column layout. Where, adter the
+    name of the table, data is written as column names followed by data
     
-    return obj
+    Arguments:
+        path {str} -- Path to the table file
+    """
+
+    tbl = figureoutTable(path)
+
+    values = []
+    
+    with open(path, 'r') as f:
+        output = deque(f.readlines())
+
+        title = output.popleft()
+        _ = output.popleft()
+
+        columns = returnCols(
+            tbl,
+            output.popleft(),
+            output.popleft()
+        )
+
+        for line in output:
+            value = [x.strip() for x in line.split(' ')]
+            if value[-1] == '---':
+                _ = value.pop()
+                zerosToAdd = len(columns) - len(value)
+
+                value.extend([0.00]*zerosToAdd)
+
+            values.append(value)
+
+    DF = pd.DataFrame(data=values, columns=columns)
+    return DF
 
 
-def extractSystemMain(inputfiles):
+def extractGeneric(path):
+    """Reads a table file which as a generic column layout. Where, adter the
+    name of the table, data is written as column names followed by data
+    
+    Arguments:
+        path {str} -- Path to the table file
+    """
+
+    tbl = figureoutTable(path)
+
+    values = []
+    
+    with open(path, 'r') as f:
+        output = deque(f.readlines())
+
+        title = output.popleft()
+        _ = output.popleft()
+
+        columns = returnCols(
+            tbl,
+            output.popleft(),
+            output.popleft()
+        )
+
+        for line in output:
+            values.append([value.strip() for value in line.split(' ') if value.strip()])
+
+    DF = pd.DataFrame(data=values, columns=columns)
+    return DF
+
+
+def extractSystemMain(path):
     """Reads the file System_main_tbl.txt and returns a fataframe object 
     containing the information
     
     Arguments:
         inputfiles {dict} -- Dictionary of all the input files
     """
-    
-    phase_main = inputfiles['phase_main']
-    system_main = inputfiles['system_main']
+
     values = []
+    
+    tbl = figureoutTable(path)
 
-    with open(system_main) as f:
+    with open(path) as f:
         output = deque(f.readlines())
+        
+        Title = output.popleft()
+        _ = output.popleft()
 
-        output = findDescriptor(output)
-        columns = returnCols(
-            output.popleft(),                   # Passing first two Non Blank
-            output.popleft()                    # lines
-        )
+        columns = returnCols(tbl, output.popleft(), output.popleft())
 
         for line in output:
             values.append([value.strip() for value in line.split()])
@@ -174,86 +272,73 @@ def extractSystemMain(inputfiles):
     return DF
 
 
-def extractPhaseMain(inputfiles, F):
+def extractPhaseMain(phase_main, F):
     """Extracts the Phase Data, and also adds remaining melt data to the 
     DataFrame
     
     Arguments:
-        inputfiles {dict} -- Dictionary of the input files.
+        phase_main -- path to the input file containing Phase related Data
         F {list} -- List of F (remaining melt fraction) values for all the 
             temperatures
     """
-
-    phase_main = inputfiles['phase_main']
-    system_main = inputfiles['system_main']
+    tbl = figureoutTable(phase_main)
 
     with open(phase_main, 'r') as f:
         output = deque(f.readlines())
-        iteration = []
-
-        output = findDescriptor(output)
+        title = output.popleft()
+        _ = output.popleft()
         
-        columns = returnCols(output[0], None)   # Passing the first non-Blank 
-        #                                         Line
-        
-        DF = pd.DataFrame(columns=columns)      # Empty Dataframe with required
-        #                                         columns
-
-        # Getting the values for Melt Remaining (f), Expecting a list of values
-        fvals = F
-
-        # With every append, this goes up by one, hence can be effectively used 
-        # for accessing the correct f value fro the given PT condition
-        faccessor = 0
-
-        while(len(output) > 0):
+        if tbl == 1:
+            # List for storing all the DataFrames
+            phaseDFs = []
+            phase = deque()
             
-            line = output.popleft().strip("\n")
+            while len(output) > 0:
+                currentLine = output.popleft()
+                if currentLine.strip() != '':
+                    phase.append(currentLine)
+                else:
+                    phaseDFs.append(_extractPhaseMainT1(phase))
+                    phase = deque()
+        
+            DF = pd.concat(phaseDFs, join='outer', sort=False)
+            DF = DF.sort_values('Temperature', ascending=False)
 
-            if line.split(" ")[0] != "Pressure":
-                iteration.append(line)
-            elif line.split(" ")[0] == "Pressure":
-                if len(iteration) != 0:
-                    # Read the data and fill No data Values with None
-                    currentEnv = _extractPhaseData(
-                        iteration,
-                        fvals[faccessor],
-                        columns
-                    )
-                    # Convert this to pandas DataFrame
-                    DF = DF.append(currentEnv)
-                # Next iteration
-                iteration = [line]
-                faccessor += 1
+        elif tbl == 2:
+            iteration = []
+            
+            columns = returnCols(tbl, output[0], None)
+            
+            DF = pd.DataFrame(columns=columns)
+
+            # Getting the values for Melt Remaining (f)
+            fvals = F
+
+            # With every append, this goes up by one, hence can be effectively used 
+            # for accessing the correct f value fro the given PT condition
+            faccessor = 0
+
+            while(len(output) > 0):
+                
+                line = output.popleft().strip("\n")
+
+                if line.split(" ")[0] != "Pressure":
+                    iteration.append(line)
+                elif line.split(" ")[0] == "Pressure":
+                    if len(iteration) != 0:
+                        # Read the data and fill No data Values with None
+                        currentEnv = _extractPhaseDataT2(
+                            iteration,
+                            fvals[faccessor],
+                            columns
+                        )
+                        # Convert this to pandas DataFrame
+                        DF = DF.append(currentEnv)
+                    # Next iteration
+                    iteration = [line]
+                    faccessor += 1
 
         return DF
-
-
-def extractBulkComp(inputfiles):
-    """Extracts the data from Bulk_comp_tbl.txt file
-    
-    Arguments:
-        inputfiles {dict} -- dict containing all the input file paths
-    """
-    bulk_comp = inputfiles["bulk_comp"]
-
-    values = []
-    
-    with open(bulk_comp, 'r') as f:
-        output = deque(f.readlines())
-
-        output = findDescriptor(output)
-
-        columns = returnCols(
-            output.popleft(),
-            output.popleft()
-        )
-
-        for line in output:
-            values.append([value.strip() for value in line.split(' ')])
-
-    DF = pd.DataFrame(data=values, columns=columns)
-    return DF
 
 
 def extractData(inputfiles):
@@ -263,13 +348,48 @@ def extractData(inputfiles):
     Arguments:
         inputfiles {dict} -- Dictionary of all the input files
     """
+    # Guide to keys of data
+    # 'phase_main', Done
+    # 'phase_mass', Done [Generic]
+    # 'phase_vol', Done [Generic]
+    # 'solid_comp', Done
+    # 'system_main', Done
+    # 'trace_main', Not Yet
+    # 'bulk_comp' Done [Generic]
 
-    SystemMain = extractSystemMain(inputfiles)
-    F = SystemMain['F'].values
+    print("[+] Extracting System Thermodynamic Data")
+    systemMain = extractSystemMain(inputfiles['system_main'])
+    F = systemMain['F'].values
+    print("[+] Extracted System Thermodynamic Data Successfully")
 
-    PhaseMain = extractPhaseMain(inputfiles, F)
-    
-    BulkMain = extractBulkComp(inputfiles)
+    print("[+] Reading Phase Main Data")
+    phaseMain = extractPhaseMain(inputfiles['phase_main'], F)
+    print("[+] Read Phase Main Data successfully")
+
+    print("[+] Extracting Bulk Composition Data")
+    bulkComp = extractGeneric(inputfiles['bulk_comp'])
+    print("[+] Extracted Bulk Composition Data Successfully")
+
+    print("[+] Extracting Phase Mass data")
+    phaseMass = extractGeneric(inputfiles['phase_mass'])
+    print("[+] Extracted Phase Mass Data Successfully")
+
+    print("[+] Extract Solid Composition Data")
+    solidComp = extractSolidComp(inputfiles['solid_comp'])
+    print("[+] Extracted Solid Composition Data Successfully")
+
+    print("[+] Extracting Phase Volume Data")
+    phaseVol = extractGeneric(inputfiles['phase_vol'])
+    print("[+] Extracted Phase Volume Data Successfully")
+
+    Data = {
+        'phase_main': phaseMain,
+        'phase_mass': phaseMass,
+        'phase_vol': phaseVol,
+        'solid_comp': solidComp,
+        'system_main': systemMain,
+        'bulk_comp': bulkComp
+    }
 
     convertTemp = input("[?] Do you want to convert Temperatures to C from K (y/n): ")
 
@@ -279,11 +399,13 @@ def extractData(inputfiles):
         convertTemp = False
 
     if convertTemp:
-        PhaseMain['Temperature'] = PhaseMain['Temperature'].astype(float) - 273.15
-        BulkMain['Temperature'] = BulkMain['Temperature'].astype(float) - 273.15
-        SystemMain['Temperature'] = SystemMain['Temperature'].astype(float) - 273.15   
+        # PhaseMain['Temperature'] = PhaseMain['Temperature'].astype(float) - 273.15
+        # BulkMain['Temperature'] = BulkMain['Temperature'].astype(float) - 273.15
+        # SystemMain['Temperature'] = SystemMain['Temperature'].astype(float) - 273.15   
+        for key in Data.keys():
+            Data[key]['Temperature'] = Data[key]['Temperature'].astype(float) - 273.15
 
-    return (PhaseMain, SystemMain, BulkMain)
+    return Data    
 
 
 def extractDirName(filepath):
@@ -321,18 +443,27 @@ def moveTables(mainpath, outputDir):
         os.rename(origin, destination)
 
 
-def writeCSV(filepath, DF):
-    """Writes a DataFrame file to the filepath given as a csv file
+def writeCSV(data, outputDir):
+    """
+    Takes in a dictionary of DataFrames with the names of their proposed file
+    nanmes as the keys to the DataFrame
     
     Arguments:
-        filepath {str} -- Complete path of the csv file
-        DF {pandas.DataFrame} -- The DataFrame object that is to be saved
+        data: dictionary of DataFrames with the names of their files as the
+            keys
+        outputDir: Path to the directory where the CSV files are to saved
     """
-    if not os.path.isdir(extractDirName(filepath)):
-        os.makedirs(extractDirName(filepath))
-    with open(filepath, 'w') as out:
-        print("Writing CSV at: {}".format(filepath))
-        DF.to_csv(out)
+
+    if not os.path.isdir(outputDir):
+        os.makedirs(outputDir)
+
+    for key in data.keys():
+        filename = key + ".csv"
+        outputPath = os.path.join(outputDir, filename)
+    
+        with open(outputPath, 'w') as out:
+            print("[+] Writing CSV at: {}".format(outputPath))
+            data[key].to_csv(out)
 
 
 def getArgs():
@@ -355,15 +486,72 @@ def getArgs():
     return inputfile
 
 
+def figureoutTable(filepath):
+    """Figures out whoch table is this. Reads the file, and using regex mathes,
+    returns the value corresponding to the type of table the file contains
+    
+    Arguments:
+        filepath {str} -- path of the file containing the table
+    """
+    with open(filepath) as f:
+        output = deque(f.readlines())
+        Title = output.popleft()
+
+        _ = output.popleft()
+
+        # Guide:
+        # 1. phase_main_tbl.txt (type 1) -- Starts with liquid_0 thermodynamic..
+        # 2. phase_main_tbl.txt (type 2) -- Starts with P T ...
+        # 3. phase_mass_tbl.txt -- starts with Phase Masses
+        # 4. phase_vol_tbl.txt -- starts with Phase Volumes
+        # 5. solid_comp_tbl.txt -- starts with Solid Composition
+        # 6. system_mail_tbl.txt -- starts with System Thermodynamic data
+        # 7. trace_main_tbl.txt -- starts as P Pval T Tval
+        # 8. bulk_comp_tbl.txt -- starts with Bulk Composition
+
+        tbl = None
+        if bool(re.search(r'[a-z]+_[0-9] ([a-z]+[ |:])+', output[0])):
+            # phase_main_tbl (type 1)
+            tbl = 1
+        elif bool(re.search(r'^([A-Za-z]+ [0-9]+\.?[0-9]{2}? ){2}\b', output[0])):
+            # phase_main_tbl (type 2)
+            tbl = 2
+        elif bool(re.search(r'Phase Masses:',output[0])):
+            # phase_mass_tbl
+            tbl = 3
+        elif bool(re.search(r'Phase Volumes:', output[0])):
+            # phase_vol_tbl
+            tbl = 4
+        elif bool(re.search(r'Solid Composition:', output[0])):
+            # solid_comp_tbl
+            tbl = 5
+        elif bool(re.search(r'System Thermodynamic Data:', output[0])):
+            # system_main_tbl
+            tbl = 6
+        elif bool(re.search(r'^([A-Za-z]+ [0-9]+\.?[0-9]{2}? ?){2}$', output[0])):
+            tbl = 7
+        elif bool(re.search(r'Bulk Composition:', output[0])):
+            tbl = 8
+        else:
+            tbl = 0
+
+    return tbl
+
+
 if __name__ == '__main__':
     # Input the location where the data files are present
     if len(sys.argv) > 1:
         mainpath = getArgs()
     else:
-        mainpath = input("Enter path to output files (Leave blank for default): ")
+        mainpath = input("[!] Enter path to Working Directory (Press ENTER for default): ")
         if mainpath == '':
             mainpath = '../'
     
+    # Check if path is valid
+    if not os.path.isdir(mainpath):
+        print("[-] Working Directory doesn't exist; Exiting")
+        sys.exit(2)
+
     outputpath = mainpath + "/Scripts/output/{}/".format(
         dt.now().strftime('%Y-%m-%d_%H-%M')
     )
@@ -376,35 +564,31 @@ if __name__ == '__main__':
     phase_mass = mainpath + "Phase_mass_tbl.txt"    # redundant
     phase_vol = mainpath + "Phase_vol_tbl.txt"      # redundant
     trace_main = mainpath + "Trace_main_tbl.txt"
-    
-
+  
     inputpaths = (
         phase_main,
-        system_main,
-        bulk_comp,
+        phase_mass,
+        phase_vol,
         solid_comp,
-        trace_main
+        system_main,
+        trace_main,
+        bulk_comp
     )
     inputnames = (
         'phase_main',
-        'system_main',
-        'bulk_comp',
+        'phase_mass',
+        'phase_vol',
         'solid_comp',
-        'trace_main'
+        'system_main',
+        'trace_main',
+        'bulk_comp'
     )
 
     inputfiles = dict(zip(inputnames, inputpaths))
-   
-    phaseOutput = outputpath + 'Phase_main_tbl.csv'
-    systemOutput = outputpath + 'System_main_tbl.csv'
-    bulkOutput = outputpath + 'Bulk_comp_tbl.csv'
     
-    PhaseMain, SystemMain, BulkComp = extractData(inputfiles)
+    Data = extractData(inputfiles)
 
-    writeCSV(phaseOutput, PhaseMain)
-    writeCSV(systemOutput, SystemMain)
-    writeCSV(bulkOutput, BulkComp)
+    writeCSV(Data, outputpath)
 
     # Cleaning the directory
     moveTables(mainpath, outputpath)
-
